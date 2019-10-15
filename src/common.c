70eac406c2c4cfb255e9a237eebb143911337b5e
dead end <dead.end@inbox.ru>
2019-10-15 23:06:11 +0200

    definition of the common source file (currently not used)

---
+++ src/common.c
@@ -0,0 +1,18 @@
+#include "common.h"
+
+/******************************************************************************
+ * The function allocates memory and terminates the program in case of an
+ * error.
+ *****************************************************************************/
+
+void* xmalloc(const size_t size) {
+
+	void *ptr = malloc(size);
+
+	if (ptr == NULL) {
+		log_exit("Unable to allocate: %zu bytes of memory!", size);
+	}
+
+	return ptr;
+}
+
