15578e773f10667d3107745d149245d8a83d55f7
dead end <dead.end@inbox.ru>
2019-10-29 21:29:45 +0100

    moved bg function to a separate file

---
+++ inc/bg_area.h
@@ -0,0 +1,9 @@
+#ifndef INC_BG_AREA_H_
+#define INC_BG_AREA_H_
+
+#include "colors.h"
+#include "s_area.h"
+
+void bg_area_print_pixel(const s_point *pixel, const enum e_colors color, const wchar_t chr);
+
+#endif /* INC_BG_AREA_H_ */
---
+++ src/bg_area.c
@@ -0,0 +1,19 @@
+#include <ncurses.h>
+
+#include "common.h"
+#include "colors.h"
+#include "s_area.h"
+
+/******************************************************************************
+ * The background area has only one function, which prints a pixel with a given
+ * color and wide character.
+ *****************************************************************************/
+
+void bg_area_print_pixel(const s_point *pixel, const enum e_colors color, const wchar_t chr) {
+
+	colors_bg_attr(color);
+
+	mvprintw(pixel->row, pixel->col, "%lc", chr);
+
+	log_debug("row: %d col: %d color: %d", pixel->row, pixel->col, color);
+}
