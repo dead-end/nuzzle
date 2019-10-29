ec3ba7a7a8eaa29a06ed1f40641aa6fb920f7d8e
dead end <dead.end@inbox.ru>
2019-10-29 21:30:08 +0100

    dummy files

---
+++ inc/game_area.h
@@ -0,0 +1,6 @@
+#ifndef INC_GAME_AREA_H_
+#define INC_GAME_AREA_H_
+
+void game_area_init();
+
+#endif /* INC_GAME_AREA_H_ */
---
+++ src/game_area.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+void game_area_init() {
+	log_debug_str("coming soon...");
+}
