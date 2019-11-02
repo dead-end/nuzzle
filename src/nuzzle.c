669f593bb6fd5a47773d4e68c96f0f25719a84a0
dead end <dead.end@inbox.ru>
2019-11-03 00:47:21 +0100

    added license to all files

--- src/nuzzle.c
+++ src/nuzzle.c
@@ -1,19 +1,44 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include <ncurses.h>
 #include <locale.h>
 #include <time.h>
 
-#include "common.h"
-#include "colors.h"
 #include "info_area.h"
 #include "game_area.h"
 #include "bg_area.h"
 #include "new_area.h"
 
+#include "colors.h"
+#include "common.h"
+
 #define ADD_SIZE 3
 
 s_point abs_home = { .row = 10, .col = 60 };
 
-s_point abs_old = { .row = 10, .col = 60 };
+//s_point abs_old = { .row = 10, .col = 60 };
 
 /******************************************************************************
  *
@@ -100,8 +125,6 @@ int main() {
 	curs_set(0);
 
 	refresh();
-
-//	game_area_pos("Start while");
 
 	for (;;) {
 		int c = wgetch(stdscr);
