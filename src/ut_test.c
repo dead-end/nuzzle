933a09a1297692323a99c0a2fd00afc25dfd9dd3
dead end <dead.end@inbox.ru>
2020-06-02 12:55:39 +0200

    added unit tests for rules

--- src/ut_test.c
+++ src/ut_test.c
@@ -25,6 +25,7 @@
 #include <stdlib.h>
 
 #include "ut_s_area.h"
+#include "ut_rules.h"
 
 /******************************************************************************
  * The main function delegates the call to the individual unit test functions.
@@ -33,6 +34,8 @@
 int main() {
 
 	ut_s_area_exec();
+
+	ut_rules_exec();
 
 	return EXIT_SUCCESS;
 }
