826539a85986b126b2420064e5544e327aa7cbd1
dead end <dead.end@inbox.ru>
2020-06-06 13:20:46 +0200

    initial version of the files

---
+++ inc/s_game_cfg.h
@@ -0,0 +1,102 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef INC_S_GAME_CFG_H_
+#define INC_S_GAME_CFG_H_
+
+#include "common.h"
+
+/******************************************************************************
+ * The definition of the game types.
+ *****************************************************************************/
+
+#define TYPE_UNDEF -1
+
+#define TYPE_4_COLORS 1
+
+#define TYPE_SQUARES_LINES 2
+
+//
+// String values for the configuration file.
+//
+#define TYPE_4_COLORS_STR "4-colors"
+
+#define TYPE_SQUARES_LINES_STR "squares-lines"
+
+/******************************************************************************
+ * The definition of the s_game struct.
+ *****************************************************************************/
+
+#define SIZE_TITLE 32
+
+#define SIZE_DATA 1024
+
+typedef struct s_game_cfg {
+
+	// TODO: not used
+	int id;
+
+	// TODO: status
+	int type;
+
+	// TODO: not used
+	char data[SIZE_DATA];
+
+	// TODO: center
+	char title[SIZE_TITLE];
+
+	//
+	// The dimension of the game.
+	//
+	s_point game_dim;
+
+	//
+	// The dimenstion of the drop area and the home areas.
+	//
+	s_point drop_dim;
+
+	//
+	// The number of home areas.
+	//
+	int home_num;
+
+} s_game_cfg;
+
+/******************************************************************************
+ * Declaration of an array for game configurations.
+ *****************************************************************************/
+
+#define S_GAMES_CFG_MAX 4
+
+extern int s_game_cfg_num;
+
+extern s_game_cfg game_cfg[S_GAMES_CFG_MAX];
+
+/******************************************************************************
+ * Function definitions.
+ *****************************************************************************/
+
+void s_game_cfg_read(const char *path);
+
+#endif /* INC_S_GAME_CFG_H_ */
---
+++ src/s_game_cfg.c
@@ -0,0 +1,370 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 dead-end
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <linux/limits.h>
+#include <errno.h>
+#include <s_game_cfg.h>
+
+#include "common.h"
+
+/*******************************************************************************
+ * Declaration of an array for game configurations.
+ ******************************************************************************/
+
+int s_game_cfg_num = 0;
+
+s_game_cfg game_cfg[S_GAMES_CFG_MAX];
+
+/*******************************************************************************
+ * The definition of keys and tags for the configuration file.
+ ******************************************************************************/
+
+#define CFG_GAME_TAG "[game]"
+
+#define CFG_GAME_ID "game.id"
+
+#define CFG_GAME_TITLE "game.title"
+
+#define CFG_GAME_TYPE "game.type"
+
+#define CFG_GAME_TYPE_DATA "game.data"
+
+#define CFG_GAME_DIM_ROW "game.dim.row"
+
+#define CFG_GAME_DIM_COL "game.dim.col"
+
+#define CFG_DROP_DIM_ROW "drop.dim.row"
+
+#define CFG_DROP_DIM_COL "drop.dim.col"
+
+#define CFG_HOME_NUM "home.num"
+
+/*******************************************************************************
+ * The macro checks if the string starts with a prefix.
+ ******************************************************************************/
+
+#define starts_with(s,p) (strncmp(p, s, strlen(p)) == 0)
+
+/*******************************************************************************
+ * The function is called with a key value pair ("key=value") and returns a
+ * pointer to the value.
+ ******************************************************************************/
+
+static char* cfg_get_value(const char *line) {
+
+	//
+	// Get a pointer to the equals sign.
+	//
+	char *result = index(line, '=');
+
+	//
+	// Ensure that the string contains a '='.
+	//
+	if (result == NULL) {
+		log_exit("No value found in line: %s", line);
+	}
+
+	//
+	// Skip the '=' and return the result.
+	//
+	return ++result;
+}
+
+/*******************************************************************************
+ * The function is called with a key value pair ("key=value") and copies the
+ * value to an array with a given size.
+ ******************************************************************************/
+
+static inline void cfg_get_str(char *to, const char *line, const size_t size) {
+	const char *value = cfg_get_value(line);
+
+	if (strlen(value) > size - 1) {
+		log_exit("Value too long: %s", value);
+	}
+
+	strncpy(to, cfg_get_value(line), size);
+}
+
+/*******************************************************************************
+ * The function is called with a key value pair ("key=value") and returns the
+ * type of the game, which is translated from a string to an integer value.
+ ******************************************************************************/
+
+static int cfg_get_type(const char *line) {
+	const char *value = cfg_get_value(line);
+
+	if (strcmp(TYPE_4_COLORS_STR, value) == 0) {
+		return TYPE_4_COLORS;
+
+	} else if (strcmp(TYPE_SQUARES_LINES_STR, value) == 0) {
+		return TYPE_SQUARES_LINES;
+
+	} else {
+		log_exit("Unknown type: %s", line);
+	}
+}
+
+/*******************************************************************************
+ * The function is called with a key value pair ("key=value") and it is assumed
+ * that the value is an integer. It returns the value as an int.
+ ******************************************************************************/
+
+static int cfg_get_int(char *line) {
+	char *tmp;
+
+	const char *value = cfg_get_value(line);
+
+	errno = 0;
+
+	const int result = (int) strtol(value, &tmp, 10);
+
+	//
+	// Check for overflows.
+	//
+	if (errno != 0) {
+		log_exit("Unable to convert: %s - %s", value, strerror(errno));
+	}
+
+	//
+	// Ensure that the whole value was converted.
+	//
+	if (*tmp != '\0') {
+		log_exit("Unable to convert: %s - %s", value, tmp);
+	}
+
+	return result;
+}
+
+/*******************************************************************************
+ * The function prints a game config structure.
+ ******************************************************************************/
+
+static void s_game_debug(const s_game_cfg *game_cfg) {
+
+	log_debug("id: %d", game_cfg->id);
+	log_debug("title: %s", game_cfg->title);
+	log_debug("type: %d", game_cfg->type);
+	log_debug("data: %s", game_cfg->data);
+	log_debug("game dim: %d/%d", game_cfg->game_dim.row, game_cfg->game_dim.col);
+	log_debug("drop dim: %d/%d", game_cfg->drop_dim.row, game_cfg->drop_dim.col);
+	log_debug("home num: %d", game_cfg->home_num);
+
+}
+
+/*******************************************************************************
+ * The function initializes all values for the s_game structures with invalid
+ * values. All invalid values have to be overwritten by the configuration file,
+ * so we can check if the configuration file is complete.
+ ******************************************************************************/
+
+static void s_game_init() {
+
+	for (int i = 0; i < S_GAMES_CFG_MAX; i++) {
+
+		game_cfg[i].id = -1;
+
+		game_cfg[i].title[0] = '\0';
+
+		game_cfg[i].type = TYPE_UNDEF;
+
+		game_cfg[i].data[0] = '\0';
+
+		s_point_set(&game_cfg[i].game_dim, -1, -1);
+
+		s_point_set(&game_cfg[i].drop_dim, -1, -1);
+
+		game_cfg[i].home_num = -1;
+	}
+}
+
+/*******************************************************************************
+ * The function checks if all values of the game structure are valid.
+ ******************************************************************************/
+
+static void s_game_check() {
+
+	//
+	// Ensure that we have at least one game configuration.
+	//
+	if (s_game_cfg_num <= 0) {
+		log_exit_str("No games defined!");
+	}
+
+	//
+	// We check only the defined s_game structures.
+	//
+	for (int i = 0; i < s_game_cfg_num; i++) {
+
+		if (game_cfg[i].id < 0) {
+			log_exit("Game: %d -not set: '%s'", i, CFG_GAME_ID);
+		}
+
+		if (game_cfg[i].title[0] == '\0') {
+			log_exit("Game: %d - not set: '%s'", i, CFG_GAME_TITLE);
+		}
+
+		if (game_cfg[i].type == TYPE_UNDEF) {
+			log_exit("Game: %d -not set: '%s'", i, CFG_GAME_TYPE);
+		}
+
+		if (game_cfg[i].data[0] == '\0') {
+			log_exit("Game: %d - not set: '%s'", i, CFG_GAME_TYPE_DATA);
+		}
+
+		if (game_cfg[i].game_dim.row < 0 || game_cfg[i].game_dim.col < 0) {
+			log_exit("Game: %d - not set: '%s' or '%s'", i, CFG_GAME_DIM_ROW, CFG_GAME_DIM_COL);
+		}
+
+		if (game_cfg[i].drop_dim.row < 0 || game_cfg[i].drop_dim.col < 0) {
+			log_exit("Game: %d - not set: '%s' or '%s'", i, CFG_DROP_DIM_ROW, CFG_DROP_DIM_COL);
+		}
+
+		if (game_cfg[i].home_num < 0) {
+			log_exit("Game: %d - not set: '%s'", i, CFG_HOME_NUM);
+		}
+	}
+}
+
+/*******************************************************************************
+ * The function reads the configurations from the config file to an array of
+ * s_game_cfg structures.
+ ******************************************************************************/
+
+#define BUF_SIZE 1024
+
+void s_game_cfg_read(const char *path) {
+	char line[BUF_SIZE];
+	s_game_cfg *game = NULL;
+
+	//
+	// Initialize the game configurations. This is required by the
+	// s_game_check() function to be able to check if all configurations are
+	// complete
+	//
+	s_game_init();
+
+	//
+	// Open the score file.
+	//
+	FILE *file = fopen(path, "r");
+	if (file == NULL) {
+		log_exit("Unable open file: %s - %s", path, strerror(errno));
+	}
+
+	//
+	// Read the file line by line.
+	//
+	while (fgets(line, BUF_SIZE, file) != NULL) {
+
+		//
+		// Ensure that the IO operation succeeded.
+		//
+		if (ferror(file)) {
+			log_exit("Unable to read file: %s - %s", path, strerror(errno));
+		}
+
+		//
+		// Remove tailing white spaces.
+		//
+		trim_r(line);
+
+		//
+		// Ignore empty lines and comments.
+		//
+		if (strlen(line) == 0 || line[0] == '#') {
+			continue;
+		}
+
+		log_debug("line: %s", line);
+
+		//
+		// The game tag signals the start of a new game configuration.
+		//
+		if (starts_with(line, CFG_GAME_TAG)) {
+
+			if (s_game_cfg_num >= S_GAMES_CFG_MAX) {
+				log_exit_str("Too many games!");
+			}
+
+			s_game_cfg_num++;
+			game = &game_cfg[s_game_cfg_num - 1];
+			continue;
+		}
+
+		//
+		// Ensure that game is defined, to be able to set game data.
+		//
+		if (game != NULL) {
+
+			if (starts_with(line, CFG_GAME_ID)) {
+				game->id = cfg_get_int(line);
+
+			} else if (starts_with(line, CFG_GAME_TITLE)) {
+				cfg_get_str(game->title, line, SIZE_TITLE);
+
+			} else if (starts_with(line, CFG_GAME_TYPE)) {
+				game->type = cfg_get_type(line);
+
+			} else if (starts_with(line, CFG_GAME_TYPE_DATA)) {
+				cfg_get_str(game->data, line, SIZE_DATA);
+
+			} else if (starts_with(line, CFG_GAME_DIM_ROW)) {
+				game->game_dim.row = cfg_get_int(line);
+
+			} else if (starts_with(line, CFG_GAME_DIM_COL)) {
+				game->game_dim.col = cfg_get_int(line);
+
+			} else if (starts_with(line, CFG_DROP_DIM_ROW)) {
+				game->drop_dim.row = cfg_get_int(line);
+
+			} else if (starts_with(line, CFG_DROP_DIM_COL)) {
+				game->drop_dim.col = cfg_get_int(line);
+
+			} else if (starts_with(line, CFG_HOME_NUM)) {
+				game->home_num = cfg_get_int(line);
+
+			} else {
+				log_exit("Unknown definition: %s", line);
+			}
+
+		} else {
+			log_exit("Unknown definition: %s", line);
+		}
+	}
+
+	fclose(file);
+
+#ifdef DEBUG
+
+	for (int i = 0; i < s_game_cfg_num; i++) {
+		s_game_debug(&game_cfg[i]);
+	}
+#endif
+
+	//
+	// Ensure that all game configurations are complete.
+	//
+	s_game_check();
+}
