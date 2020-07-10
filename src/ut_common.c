81f44eed5171e3d9f00beed28d92c3fc38258aa1
dead end <dead.end@inbox.ru>
2020-07-10 23:00:28 +0200

    added unit tests

--- src/ut_common.c
+++ src/ut_common.c
@@ -89,6 +89,43 @@ static void test_cpy_str_centered() {
 }
 
 /******************************************************************************
+ * The function checks the cp_box_str() function.
+ *****************************************************************************/
+
+#define BUF_STR 11
+
+static void test_cp_box_str() {
+	wchar_t buf[BUF_STR];
+	wchar_t tmp[BUF_STR];
+
+	swprintf(tmp, BUF_STR, L"%lc 1234   %lc", U_VLINE, U_VLINE);
+
+	cp_box_str(L"1234", buf, BUF_STR);
+	ut_check_wstr(buf, tmp, "1234");
+
+	swprintf(tmp, BUF_STR, L"%lc 123456 %lc", U_VLINE, U_VLINE);
+
+	cp_box_str(L"123456", buf, BUF_STR);
+	ut_check_wstr(buf, tmp, "123456");
+}
+
+/******************************************************************************
+ * The function checks the cp_box_line() function.
+ *****************************************************************************/
+
+#define BUF_LINE 6
+
+static void test_cp_box_line() {
+	wchar_t buf[BUF_LINE];
+
+	const wchar_t tmp[] = { U_ULCORNER, U_HLINE, U_HLINE, U_HLINE, U_URCORNER, L'\0' };
+
+	cp_box_line(buf, BUF_LINE, U_ULCORNER, U_URCORNER, U_HLINE);
+
+	ut_check_wstr(buf, tmp, "upper");
+}
+
+/******************************************************************************
  * The function is the a wrapper, that triggers the internal unit tests.
  *****************************************************************************/
 
@@ -99,4 +136,8 @@ void ut_common_exec() {
 	test_trim_r();
 
 	test_cpy_str_centered();
+
+	test_cp_box_str();
+
+	test_cp_box_line();
 }
