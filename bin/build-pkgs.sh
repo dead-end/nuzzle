daa8efd403998fc7cef8ec9029c3af230ac24cf5
dead end <dead.end@inbox.ru>
2020-07-19 18:05:02 +0200

    initial versions

---
+++ bin/build-pkgs.sh
@@ -0,0 +1,199 @@
+#/bin/sh
+
+################################################################################
+# The script builds a tar ball with the ccsvv sources or a debian .deb package.
+################################################################################
+
+set -ue
+
+arch="amd64"
+
+version_header="inc/common.h"
+
+program="nuzzle"
+
+################################################################################
+# Error function.
+################################################################################
+
+do_exit() { 
+  echo "ERROR: ${1}" 
+  exit 1
+}
+
+################################################################################
+# Ensure that we are in the correct directory.
+################################################################################
+
+if [ ! -d "build/" ] ; then
+  do_exit "The directory: build/ does not exist!"
+fi
+
+################################################################################
+# The load the version to the variable: version
+################################################################################
+
+version=$(sed -n 's/#define VERSION "\([^"]*\)"/\1/p' ${version_header})
+
+if [ "${version}" = "" ] ; then
+  do_exit "No version found!"
+fi
+
+echo "Version: ${version}"
+
+################################################################################
+# The function prints a usage / help message. If it is called with an argument
+# this argument is interpreted as an error message and the function terminates
+# the script with an exit code of 1. Otherwise the function terminates the 
+# script normally.
+################################################################################
+
+usage() {
+  echo "Usage: ${0} [help|src|deb"
+  echo "  help : Prints this message"
+  echo "  src  : Creates a tarball with the ${program} sources"
+  echo "  deb  : Creates a debian package"
+
+  if [ "${#}" != "0" ] ; then
+    echo "\nERROR: ${1}"
+    exit 1
+  else
+    exit 0
+  fi
+}
+
+################################################################################
+# The function builds a tarball with the program sources. The result is: 
+#
+#   ${program}-{version}.tar.gz 
+#
+# and the build should work with the tarball:
+#
+# > tar xvzf ${program}-{version}.tar.gz
+# > cd ${program}-{version}/
+# > make
+# > make install
+################################################################################
+
+create_src() {
+
+  #
+  # Remove old directories
+  #
+  if [ -d "build/${program}-${version}" ] ; then
+    rm -rf "build/${program}-${version}/"
+  fi
+
+  #
+  # Create the root directory for the build
+  #
+  root_dir="build/${program}-${version}"
+
+  #
+  # Create the empty directories. The original directories may contain .md
+  # files.
+  #
+  mkdir -p "${root_dir}/build/" "${root_dir}/tests/" || do_exit "Unable to create directories"
+
+  cp -r src/ inc/ man/ makefile LICENSE "${root_dir}/" || do_exir "Unable to copy data"
+
+  #
+  # Create the tar ball
+  #
+  tar cvzf "build/${program}-${version}.tar.gz" "${root_dir}/" || do_exit "Unable to create final tar"
+}
+
+################################################################################
+# The function builds a debian .deb package.
+################################################################################
+
+create_deb() {
+
+  make clean  || do_exit "Unable to call make clean"
+
+  #
+  # Remove old directories
+  #
+  if [ -d "build/${program}_${version}_${arch}" ] ; then
+    rm -rf "build/${program}_${version}_${arch}/"
+  fi
+
+  #
+  # Create the root directory for the build
+  #
+  root_dir="build/${program}_${version}_${arch}"
+
+  mkdir -p "${root_dir}/DEBIAN" || do_exit "Unable to create dir: DEBIAN"
+
+  #
+  # The prefix for the package
+  #
+  make PREFIX="/usr" all || do_exit "Unable to call make all"
+
+  #
+  # The prefix for the installation
+  #
+  make PREFIX="${root_dir}/usr" install || do_exit "Unable to call make install"
+
+  #
+  # Get the dependencies
+  #
+  dependencies=$(sh bin/pkg-deps.sh no-debug ${program})
+
+  echo "dependencies: ${dependencies}"
+
+  #
+  # Write the control file (${dependencies=} sets the default to "" if the
+  # variable is not defined.)
+  #
+  cat << EOF > "${root_dir}/DEBIAN/control"
+Package: ${program}
+Version: ${version}
+Priority: optional
+Section: games
+Architecture: amd64
+Homepage: https://github.com/dead-end/nuzzle
+Depends: ${dependencies=}
+Maintainer: dead-end
+Description: Curses based puzzle games
+ Nuzzle is a puzzle game collection for one player.
+EOF
+
+  #
+  # Add md5sums
+  #
+  find "${root_dir}" -type f -not -path "*DEBIAN*" | \
+    xargs md5sum | \
+    sed "s#${root_dir}/##" > "${root_dir}/DEBIAN/md5sums"
+
+  #
+  # fakeroot sets permissions and owner:group
+  #
+  fakeroot dpkg-deb --build ${root_dir} || do_exit "dpkg-deb"
+}
+
+################################################################################
+# Main program
+################################################################################
+
+if [ "${#}" != "1" ] ; then
+  usage "Mode is missing"
+fi
+
+mode="${1}"
+
+if [ "${mode}" = "help" ] ; then
+  usage
+
+elif [ "${mode}" = "src" ] ; then
+  create_src
+
+elif [ "${mode}" = "deb" ] ; then
+  create_deb
+
+else
+  usage "Unknown mode: ${mode}"
+fi
+
+exit 0
+
---
+++ bin/pkg-deps.sh
@@ -0,0 +1,154 @@
+#/bin/sh
+
+################################################################################
+# The script is called with an executable file and it returns a list of deb
+# packages (including the version), which the executable depends on. This
+# information can be used for the deb package build. It has a mode "no-debug"
+# or "all", which can be used to ignore the debug dependencies.
+#
+# Example:
+#
+#    >sh bin/pkg-deps.sh no-debug ccsvv
+#    libc6 (>=2.27), libncursesw5 (>=6.1), libtinfo5 (>=6.1)
+#
+#    >sh bin/pkg-deps.sh all ccsvv
+#    libc6 (>=2.27), libncursesw5 (>=6.1), libncursesw5-dbg (>=6.1), \
+#    libtinfo5 (>=6.1), libtinfo5-dbg (>=6.1)
+################################################################################
+
+set -ue
+
+DEBUG=
+
+filter=libc6-amd64
+
+################################################################################
+# A logging function that supports the log level "debug", "info", "error". The
+# function exits the script after printing an error message.
+################################################################################
+
+log () {
+  level="${1}"
+  msg="${2}"
+  
+  if [ "${level}" = "debug" ] ; then
+    if [ -n "${DEBUG}" ] ; then
+      echo "DEBUG: ${msg}"
+    fi
+    
+  elif [ "${level}" = "info" ] ; then
+    echo "INFO: ${msg}"
+
+  elif [ "${level}" = "error" ] ; then
+    echo "ERROR: ${msg}"
+    exit 1
+    
+  else
+    log "error" "EXIT: Unknown level: ${level}"
+  fi
+}
+
+################################################################################
+# The function prints the list of packages, which the executable depends on.
+################################################################################
+
+print_pkgs() {
+  dep_pkgs="${1}"
+  
+  sep=
+  result=
+
+  for dep_pkg in ${dep_pkgs} ; do
+    version=$(dpkg -s "${dep_pkg}" | grep "Version:")
+    log "debug" "version: ${version}"
+  
+    version=$(echo "${version}" | sed 's#Version: ## ; s#-.*##')
+    result="${result}${sep}${dep_pkg} (>=${version})"
+  
+    if [ -z "${sep}" ] ; then
+      sep=", "
+    fi
+  done
+  
+  echo "${result}"
+}
+
+################################################################################
+# Ensure that the script is called with an executable 
+################################################################################
+
+if [ "$#" != "2" ] ; then
+  echo "Usage: $0 [no-debug|all] <bin>"
+  exit 1
+fi
+
+mode="${1}"
+
+if [ "${mode}" != "no-debug" -a "${mode}" != "all" ] ; then
+  echo "Usage: $0 [no-debug|all] <bin>"
+  exit 1
+fi
+
+executable="${2}"
+
+if [ ! -x "${executable}" ] ; then
+  log "error" "The file: ${executable} is not an executable!"
+fi
+
+log "debug" "Processing: ${executable}"
+
+################################################################################
+# Get the list of dependencies. The script uses "raw" variables which are not
+# necessary, but they make debugging easier.
+################################################################################
+
+raw_dep_libs=$(objdump -p "${executable}")
+
+dep_libs=$(echo "${raw_dep_libs}" | grep NEEDED | sed 's# *NEEDED *##')
+
+log "debug" "dep_libs:\n${dep_libs}"
+
+dep_pkgs=
+
+for dep_lib in ${dep_libs} ; do
+
+  log "debug" "Processing: ${dep_lib}"
+  
+  pkgs_libs=$(dpkg -S ${dep_lib})
+  log "debug" "Packages and libs:\n${pkgs_libs}"
+  
+  pkgs=$(echo "${pkgs_libs}" | sed 's#:.*##')
+  log "debug" "Packages:\n${pkgs}"
+  
+  dep_pkgs="${dep_pkgs}\n${pkgs}"
+done
+
+################################################################################
+# Create a unique list of dependend packages and get the version for each 
+# package. The extention in the version is removed.
+# (before: 6.1-1ubuntu1.18.04 after: 6.1)
+################################################################################
+
+dep_pkgs=$(echo "${dep_pkgs}" | sort -u)
+
+log "debug" "dep_pkgs: ${dep_pkgs}"
+
+if [ "${mode}" = "no-debug" ] ; then
+  dep_pkgs=$(echo "${dep_pkgs}" | grep -v -- '-dbg$')
+fi
+
+log "debug" "Found dependencies with mode ${mode}:"
+
+#
+# Filter out pakages that cause issues, like: libc6-amd64
+#
+if [ "${filter}" != "" ] ; then
+  dep_pkgs=$(echo "${dep_pkgs}" | grep -v "${filter}")
+
+  log "debug" "After filter: ${filter}\n${dep_pkgs}"
+fi
+
+print_pkgs "${dep_pkgs}"
+
+exit 0
+
