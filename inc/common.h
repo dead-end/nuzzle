e083b21df556615c15033d1f687b4207b20b4421
dead end <dead.end@inbox.ru>
2021-08-08 17:56:18 +0200

    update version

--- inc/common.h
+++ inc/common.h
@@ -31,7 +31,7 @@
 #include <string.h>
 #include <wchar.h>
 
-#define VERSION "0.3.1"
+#define VERSION "0.3.2"
 
 /******************************************************************************
  * Definition of the print_debug macro. It is only defined if the DEBUG flag is
